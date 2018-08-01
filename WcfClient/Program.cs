ff1afe571a591478efb059a9f02eb4bdfe3978c4
Haken, Robert <haken@havit.cz>
2018-08-02 00:19:01 +0200

    Simple WCF (direct connection)

---
+++ WcfClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfClient
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var sw = new Stopwatch();
+
+			using (var myService = new MyServiceClient.MyServiceClient())
+			{
+				for (int i = 1; i <= 20; i++)
+				{
+					sw.Restart();
+
+					var result = myService.Echo("Blaaah!");
+
+					Console.WriteLine($"{i}\t{sw.ElapsedMilliseconds:n0}ms");
+				}
+			}
+
+			Console.ReadLine();
+			
+		}
+	}
+}
